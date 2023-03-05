35d55cfd80e3a7ed98aced5047a4fcb42ae64417
mikediessner <m.diessner2@newcastle.ac.uk>
2023-03-05 17:25:40 +0000

    updated docs

--- docs/conf.py
+++ docs/conf.py
@@ -35,3 +35,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'furo'
 html_static_path = ['_static']
 html_title = 'NUBO'
+pygments_style = "default"
+pygments_dark_style = "monokai"
